c00d84013a5aca6560eb3406a5a64aed476618e9
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-05-07 17:20:06 -0300

    mouseSituation adicionado para pintar segurando

--- js/script.js
+++ js/script.js
@@ -2,11 +2,20 @@ let range = document.getElementById("range");
 let blocksGrid = document.getElementById("blocksGrid");
 let mode;
 let selectColor = document.getElementById("inputColor");
+let mouseSituation;
 
 range.addEventListener("input", function(e) {
     gridValue = e.target.value;
     document.getElementById("resolution").innerText = "Resolution: " + gridValue + "x" + gridValue;
     createGrid(gridValue);
+});
+
+window.addEventListener("mousedown", function(e) {
+    mouseSituation = true;
+});
+
+window.addEventListener("mouseup", function(e) {
+    mouseSituation = false;
 });
 
 function createGrid(size) {
@@ -22,14 +31,16 @@ function createGrid(size) {
         
 
         createDiv.addEventListener("mouseover", function(e) {
-            if (mode === "random") {
-                let red, green, blue;
-                red = Math.floor(Math.random() * 256);
-                green = Math.floor(Math.random() * 256);
-                blue = Math.floor(Math.random() * 256);
-                e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-            } else if (mode === "color") {
-                e.target.style.backgroundColor = selectColor.value;
+            if (mouseSituation === true) {
+                if (mode === "random") {
+                    let red, green, blue;
+                    red = Math.floor(Math.random() * 256);
+                    green = Math.floor(Math.random() * 256);
+                    blue = Math.floor(Math.random() * 256);
+                    e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+                } else if (mode === "color") {
+                    e.target.style.backgroundColor = selectColor.value;
+                }
             }
         });
     }
